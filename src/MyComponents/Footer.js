f7e005696ed1d6d1a1070e5d22a2b1fe6bb44426
Pranav <pranav.21910755@viit.ac.in>
2021-07-17 23:37:43 +0530

    2nd commit

--- src/MyComponents/Footer.js
+++ src/MyComponents/Footer.js
@@ -3,7 +3,7 @@ import React from "react";
 export const Footer = () => {
   return (
     <footer className="bg-dark text-light py-3">
-      <p className="text-center">Copyright &copy; @actualpranav</p>
+      <p className="text-center"> copyright &copy; @actualpranav</p>
     </footer>
   );
 };
