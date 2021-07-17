8e3fe9ab527c122c02806d86597da2575bdc4a5d
Pranav <pranav.21910755@viit.ac.in>
2021-07-17 23:19:35 +0530

    init

---
+++ src/MyComponents/About.js
@@ -0,0 +1,10 @@
+import React from 'react'
+
+export const About = () => {
+ return (
+  <div>
+   this is an about component
+   <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Expedita nemo, officiis harum accusantium at, repudiandae, ea culpa distinctio molestias sunt numquam. Libero repudiandae temporibus laboriosam dolores iusto porro, similique optio totam, beatae voluptatibus amet? Aliquam provident repudiandae veritatis quasi eligendi et nihil quae debitis adipisci asperiores sed eum magnam, quam ullam accusantium suscipit ab dolore cumque dignissimos tenetur consequuntur alias tempore reiciendis! Veritatis illum nostrum fugit, ipsum expedita maxime pariatur.</p>
+  </div>
+ )
+}
